79641d996e9c2e8180a2487a8b0c9fbd4bfc8a90
JessP070 <jcpereira070@gmail.com>
2021-02-24 18:55:25 -0800

    add null false with rollback function

--- db/migrate/20210218184107_add_cart_to_product.rb
+++ db/migrate/20210218184107_add_cart_to_product.rb
@@ -1,5 +1,5 @@
 class AddCartToProduct < ActiveRecord::Migration[6.0]
   def change
-    add_reference :products, :cart, foreign_key: true
+    add_reference :products, :cart, null: false, foreign_key: true
   end
 end
